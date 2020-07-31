c24964d1bc12625415f18984ad3e7873bc2f098f
Simi Adebowale <adebowalesimileoluwa@gmail.com>
2020-07-31 18:40:48 -0400

    Updates

---
+++ precache-manifest.7677ed5649d5f395c7fc74b8de762fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2541ed909daf68deef8c7358edfec07a",
+    "url": "/todolist-app/index.html"
+  },
+  {
+    "revision": "92f70087693972af1345",
+    "url": "/todolist-app/static/css/main.35133e65.chunk.css"
+  },
+  {
+    "revision": "b4489659674473eb76d5",
+    "url": "/todolist-app/static/js/2.cbce6bcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-app/static/js/2.cbce6bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f70087693972af1345",
+    "url": "/todolist-app/static/js/main.e81a6f57.chunk.js"
+  },
+  {
+    "revision": "a83315ec15324cfc87cd",
+    "url": "/todolist-app/static/js/runtime-main.591f2964.js"
+  }
+]);
